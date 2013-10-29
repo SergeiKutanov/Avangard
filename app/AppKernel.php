f2c5b4adc5e966f554e6d366dc77f001175d11ea
Sergei Kutanov <sergeikutanov@gmail.com>
2013-10-29 22:59:13 +0400

    added needed bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,10 @@ class AppKernel extends Kernel
             new \Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new \Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new \Sonata\AdminBundle\SonataAdminBundle(),
+            new \Sonata\UserBundle\SonataUserBundle(),
+            new \FOS\UserBundle\FOSUserBundle(),
+            new \Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new \Application\Sonata\UserBundle\ApplicationSonataUserBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
